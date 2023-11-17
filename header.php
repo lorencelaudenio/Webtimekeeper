1ee62305e3dd7b79bbc729666c5328245478f6cb
Lorence Laudenio <63408329+lorencelaudenio@users.noreply.github.com>
2023-11-17 10:16:51 +0800

    remove logo variable

--- header.php
+++ header.php
@@ -8,7 +8,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="shortcut icon" href="" type="image/x-icon">
-    <link rel="icon" href="<?php echo $logo;?>" type="image/x-icon">
 
     <!-- DataTables Beg-->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css" />
