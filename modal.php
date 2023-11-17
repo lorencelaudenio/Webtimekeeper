8cb98a5d268c98376230147e5614d4e9da46b98e
Lorence Laudenio <63408329+lorencelaudenio@users.noreply.github.com>
2023-11-17 10:23:51 +0800

    modal rounded corner

--- modal.php
+++ modal.php
@@ -1,10 +1,10 @@
 <div class="modal fade bd-example-modal-s" tabindex="-1" role="dialog" id="privacy">
   <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
-    <div class="modal-content">
-      <div class="modal-header text-center" style="border-radius: 15px;">
+    <div class="modal-content" style="border-radius: 15px;">
+      <div class="modal-header text-center" >
         <h5 class="modal-title">Webtimekeeper <i class="bi bi-clock"></i> Privacy Policy</h5>
       </div>
-      <div class="modal-body">
+      <div class="modal-body  text-center">
         <p><small>Our website's privacy policy ensures the protection of user information collected during the access and utilization of our services. In order to provide a seamless user experience, we may collect necessary data such as usernames and passwords for the purpose of data retrieval. By using our website, users implicitly agree to comply with our privacy policy. However, users have the option to opt-out and exit the website if they choose not to comply with these policies. We prioritize the security and confidentiality of user information and strive to maintain a transparent and trustworthy relationship with our users.</small></p>
       </div>
       <div class="mb-2 text-center">
@@ -20,7 +20,7 @@
       <div class="modal-header text-center">
         <h5 class="modal-title">About Webtimekeeper <i class="bi bi-clock"></i></h5>
       </div>
-      <div class="modal-body">
+      <div class="modal-body text-center">
         <p><small>Webtimekeeper is a user-friendly time-in and time-out tracking solution designed to streamline and simplify the process. With Webtimekeeper, logging in becomes a hassle-free experience as it eliminates the complexity of traditional login systems. The application collects only essential data, including date, time-in, and timeout, on-the-fly, ensuring a seamless and efficient tracking experience. By focusing on simplicity, Webtimekeeper allows users to effortlessly record their work hours without any unnecessary complications. Say goodbye to cumbersome logins and embrace the straightforward and convenient time tracking provided by Webtimekeeper.</small></p>
       </div>
       <div class="mb-2 text-center">
