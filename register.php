e752ec2528e6108382421548d1060ddcd0d7ca3b
Lorence Laudenio <63408329+lorencelaudenio@users.noreply.github.com>
2023-11-17 17:36:01 +0800

    improve register button

--- register.php
+++ register.php
@@ -133,7 +133,7 @@ if(isset($_POST['register'])){
                     </label>
                 </div>
                 
-                <input type="submit" name="register" id="register" value="Register" class="btn btn-primary btn-lg btn-block" disabled>
+                <input type="submit" name="register" id="register" value="Register" style="text-decoration: none;" class="btn text-primary border btn-lg btn-block rounded-pill" disabled>
 
                 <p class="text-center text-muted mt-3 mb-0">Have already an account? <a href="index.php"
                     class=""><u>Timein here</u></a>
